ac325d06944eb40ce079259d9d05bd676ce350b2
OLEG M <mandrik89@gmail.com>
2020-01-28 11:37:49 +0200

    carousel

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ $(function() {
         $('.post').show(500);
     });
 });
+
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        items: 8
+    });
+});
+
+if ($(window).width() < 420) {
+    $(".owl-carousel").owlCarousel({
+        items: 3
+    })
+}
